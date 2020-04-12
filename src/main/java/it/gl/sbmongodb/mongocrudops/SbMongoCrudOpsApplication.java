90ccb40f948685bfd14de525293854c0f8e7f3bc
Giovanni Lofrumento <giovanni.lofrumento@telecomitalia.it>
2020-04-12 12:12:56 +0200

    Primo commit

---
+++ src/main/java/it/gl/sbmongodb/mongocrudops/SbMongoCrudOpsApplication.java
@@ -0,0 +1,13 @@
+package it.gl.sbmongodb.mongocrudops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMongoCrudOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMongoCrudOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gl/sbmongodb/mongocrudops/SbMongoCrudOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gl.sbmongodb.mongocrudops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMongoCrudOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
